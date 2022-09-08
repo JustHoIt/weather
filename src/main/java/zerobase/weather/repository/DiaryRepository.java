25b2962d5f875c2c573c98e895d1b31a25e446e1
Park <zalop3128@kakao.com>
2022-09-09 00:28:53 +0900

    ch4-2 0909 12:38

---
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -0,0 +1,11 @@
+package zerobase.weather.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import zerobase.weather.domain.Memo;
+
+
+@Repository
+public class DiaryRepository  {
+//    extends JpaRepository<Diary, Integer>
+}
---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,32 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class WeatherApplicationTests {
+	@Test
+	void equalTest(){
+		assertEquals(1,1); // 왼쪽 : 기댓값 //오른쪽 : 실제 값
+	}
+
+
+	@Test
+	void nullTest(){
+		assertNull(null);
+	    //given
+	    //when
+	    //then
+	}
+
+
+	@Test
+	void trueTest(){
+		assertTrue(1==1);
+	    //given
+	    //when
+	    //then
+	}
+}
