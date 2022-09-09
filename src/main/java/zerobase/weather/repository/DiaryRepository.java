45e2eb0533490b6ee9096ec9227145c10035c2b3
Park <zalop3128@kakao.com>
2022-09-10 03:07:57 +0900

    0910-0306/100802

--- src/main/java/zerobase/weather/repository/DiaryRepository.java
+++ src/main/java/zerobase/weather/repository/DiaryRepository.java
@@ -2,10 +2,29 @@ package zerobase.weather.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import zerobase.weather.domain.Memo;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Diary;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Repository
-public class DiaryRepository  {
-//    extends JpaRepository<Diary, Integer>
+public interface DiaryRepository extends JpaRepository<Diary, Integer>  {
+
+    //다이어리 조회
+    List<Diary> findAllByDate(LocalDate date);
+
+    //다이어리 조회 2
+    List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Diary getFirstByDate(LocalDate date);
+
+
+    @Transactional
+    void deleteAllByDate(LocalDate date);
+
+
+
+
 }
